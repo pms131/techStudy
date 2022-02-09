40055637ac806850e130c0b9a40573ffd4d79591
박문수 <sportssk@naver.com>
2022-02-09 20:02:49 +0900

    공부용 Spring Project 추가
    Spring Boot Project 추가

---
+++ springStudy/src/main/java/com/springstudy/di/RepositoryRank.java
@@ -0,0 +1,16 @@
+package com.springstudy.di;
+
+import java.io.IOException;
+
+public class RepositoryRank {
+
+    public int getPoint(String repositoryName) throws IOException {
+        int points = 0;
+
+        return points;
+    }
+
+    public static void main( String[] args ) {
+        RepositoryRank spring = new RepositoryRank();
+    }
+}
